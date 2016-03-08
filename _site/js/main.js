17dfe56d60a94e0347cd19f200f7107d63d3778a
Joshua Vawdrey <jvawdrey@shelbyville.lib.uts.edu.au>
2016-03-08 16:50:56 +1100

    added calculator to homepage

---
+++ _site/js/main.js
@@ -0,0 +1,122 @@
+;(function ($, window, document, undefined) {
+    /**
+     * price calculation
+     */
+
+    var calculate = function (dogs, walks, perWalk, perExtraDog) {
+        return (walks * perWalk) + ((dogs - 1) * perExtraDog);
+    };
+
+    var calculatePrice = function (dogs, walks) {
+        dogs  = parseInt(dogs || 1);
+        walks = parseInt(walks || 1);
+        
+        if (walks < 5) {
+            return calculate(dogs, walks, 25, 15);
+        }
+        else if (walks < 10) {
+            return calculate(dogs, walks, 20, 15);
+        }
+        else if (walks === 10) {
+            return calculate(dogs, walks, 18, 15);
+        }
+        else {
+            var p = calculatePrice(dogs, 10), y = walks - 10;
+            
+            while (y > 0) {
+                var i = 10;
+
+                while (i > 0) {
+                    if (y % i === 0) {
+                        p += calculatePrice(dogs, y);
+                        y -= i;
+                        break;
+                    }
+
+                    i--;
+                }
+            }
+
+            return p;
+        }
+    };
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).ready(function () {
+
+
+        $('body').on('click', '.page-scroll a', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 800, 'easeInOutQuart');
+            event.preventDefault();
+        });
+
+        // Floating label headings for the contact form
+
+        $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function () {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function () {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+
+        // Highlight the top nav as scrolling occurs
+        $('body').scrollspy({
+            target: '.navbar-fixed-top'
+        });
+
+        // Closes the Responsive Menu on Menu Item Click
+        $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function () {
+            $('.navbar-toggle:visible').click();
+        });
+
+        // form spinners
+        $('.spinner a.direction').click(function (e) {
+            e.preventDefault();
+
+            var $anchor = $(this),
+                $input = $($anchor.attr('href')),
+                dir = $anchor.data('direction').toString().toLowerCase(),
+                val = parseInt($input.val());
+
+            if (isNaN(val)) {
+                val = 0;
+            }
+
+            switch (dir) {
+                case 'down':
+                    $input.val(Math.max(1, val - 1)).change();
+                    break;
+                case 'up':
+                    $input.val(val + 1).change();
+                    break;
+            }
+
+            return false;
+        });
+
+        $('.calculator').each(function () {
+            var $calc   = $(this),
+                $target = $($calc.data('target')),
+                $dogs   = $($calc.data('dogs')),
+                $walks  = $($calc.data('walks')),
+                $disc   = $('.calculator-disclaimer', this);
+
+            $(':input', this).change(function () {
+                var price = calculatePrice($dogs.val(), $walks.val());
+                
+                if (price > 180) {
+                    $disc.html('<small>Contact us for a personalised package</small>');
+                }
+                else {
+                    $disc.text('');
+                }
+                
+                $target.text(price);
+            });
+        });
+    });
+})(jQuery, window, document);
